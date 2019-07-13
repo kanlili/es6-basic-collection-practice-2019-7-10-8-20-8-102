12df2b28a7114e0f9eb1991e2f3d077b9399be53
LILY KAN (ITA-ISDC-ISD-OOCLL/ZHA) <KANLI2@OOCL.COM>
2019-07-13 21:48:45 +0800

    upload section-1 and section-2

--- main/section-2/practice-2.js
+++ main/section-2/practice-2.js
@@ -1,5 +1,43 @@
 'use strict';
 
 module.exports = function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
-}
+        let c=[];
+          for(let item of collection){
+            if(item.length>1){
+             let {key,count}=split(item);
+             push(c,key,count);
+            }else{
+            c.push(item);
+            }
+          }
+         return count(c);
+       }
+       function split(ch){
+         let arr=ch.split("-");
+         return {key:arr[0],count:parseInt(arr[1],10)};
+       }
+       function  push(c,key,count){
+         for(var i=0;i<count;i++){
+         c.push(key);
+         }
+       }
+       function count(collection){
+          let c=[];
+            for(let item of collection){
+               let ele=find(c,item);
+               if(ele){
+                 ele.count++;
+               }else{
+               c.push({key:item,count:1});
+               }
+            }
+            return c;
+        }
+       function find(collection,ch){
+           for(let item of collection){
+                if(item.key ==ch){
+                   return item;
+                }
+           }
+            return null;
+       }
--- main/section-2/practice-3.js
+++ main/section-2/practice-3.js
@@ -1,5 +1,54 @@
 'use strict';
 
 module.exports = function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
-}
+            let c=[];
+            for(let item of collection){
+              if(item.length>1){
+               let {key,count}=split(item);
+               push(c,key,count);
+              }else{
+              c.push(item);
+              }
+            }
+           return count(c);
+         }
+         function split(ch){
+          if(ch.includes('-')){
+            let arr=ch.split('-');
+            return {key:array[0],count:parseInt(arr[1],10)};
+            }
+          if(ch.includes(":")){
+             let arr=ch.split(":");
+             return {key:array[0],count:parseInt(arr[1],10)};
+           }
+          if(ch.includes("[")){
+             let name = ch.charAt(0);
+             let summary = parseInt(ch.substr(2,ch.length-1));
+             return {name,summary};
+            }
+         }
+         function  push(c,key,count){
+           for(var i=0;i<count;i++){
+           c.push(key);
+           }
+         }
+         function count(collection){
+            let c=[];
+              for(let item of collection){
+                 let ele=find(c,item);
+                 if(ele){
+                   ele.count++;
+                 }else{
+                 c.push({key:item,count:1});
+                 }
+              }
+              return c;
+          }
+         function find(collection,ch){
+             for(let item of collection){
+                  if(item.key ==ch){
+                     return item;
+                  }
+             }
+              return null;
+         }
